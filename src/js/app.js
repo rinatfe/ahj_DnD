b8df12b838135074c635415067fb446447f4533d
Rinat Kamaletdinov <sky2208@ya.ru>
2021-06-04 13:13:57 +0400

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -7,11 +7,28 @@ class TaskBoard {
     this.button = document.querySelectorAll('.adding-list');
     this.field = document.querySelector('.field');
     this.create = document.querySelector('.create');
+    this.close = document.querySelector('.close');
+    this.board = document.querySelector('.board');
+
+    this.onLoad = this.onLoad.bind(this);
+    this.dragEnd = this.dragEnd.bind(this);
+    this.createItem = this.createItem.bind(this);
+    this.openModal = this.openModal.bind(this);
+  }
+
+  onLoad() {
+    if (localStorage.length) {
+      document.addEventListener('DOMContentLoaded', () => {
+        this.board.innerHTML = '';
+        this.board.innerHTML = localStorage.getItem('test');
+      });
+    } else { /* eslint-disable */
+      return;
+    }
   }
 
   onDrag() {
     document.addEventListener('mousedown', (e) => {
-      e.preventDefault();
       if (!e.target.classList.contains('content')) {
         return;
       }
@@ -22,13 +39,17 @@ class TaskBoard {
       this.ghostEl.style.width = window.getComputedStyle(e.target.parentNode).getPropertyValue('width');
       this.ghostEl.style.left = `${e.pageX - this.ghostEl.offsetWidth / 2.5}px`;
       this.ghostEl.style.top = `${e.pageY - this.ghostEl.offsetHeight / 2}px`;
+      this.ghostEl.style.cursor = 'grabbing';
       document.body.appendChild(this.ghostEl);
     });
   }
 
   onMove() {
     document.addEventListener('mousemove', (e) => {
-      e.preventDefault();
+      if (e.target.classList.contains('content')) {
+        e.target.firstElementChild.style.display = 'block';
+      }
+
       if (!this.draggedEl) {
         return;
       }
@@ -37,9 +58,18 @@ class TaskBoard {
     });
   }
 
+  onLeave() { /* eslint-disable */
+    document.addEventListener('mouseout', (e) => {
+      if (e.target.classList.contains('content') && !document.elementFromPoint(e.pageX, e.pageY).classList.contains('remove-item')) {
+        e.target.firstElementChild.style.display = 'none';
+      } else {
+        return;
+      }
+    });
+  }
+
   dragEnd() {
     document.addEventListener('mouseup', (e) => {
-      e.preventDefault();
       if (!this.draggedEl) {
         return;
       }
@@ -51,6 +81,7 @@ class TaskBoard {
         document.body.removeChild(this.ghostEl);
         this.ghostEl = null;
         this.draggedEl = null;
+        localStorage.setItem('test', this.board.innerHTML);
       } else {
         document.body.removeChild(this.ghostEl);
         this.ghostEl = null;
@@ -62,29 +93,52 @@ class TaskBoard {
   moving() {
     this.onDrag();
     this.onMove();
+    this.onLeave();
     this.dragEnd();
   }
 
   openModal() {
-    this.button.forEach((x) => {
-      x.addEventListener('click', (e) => {
+    document.addEventListener('click', (e) => {
+      if (e.target.classList.contains('adding-list')) {
         this.modal.style.display = 'block';
         this.currenciesBlock = document.querySelector(`[data-id = "${e.target.parentElement.dataset.number}"]`);
-      });
+      }
+    });
+  }
+
+  closeModal() {
+    document.addEventListener('click', (e) => {
+      if (e.target.classList.contains('close')) {
+        this.modal.style.display = 'none';
+      }
     });
   }
 
   createItem() {
     this.openModal();
-    this.create.addEventListener('click', (e) => {
-      e.preventDefault();
+    this.closeModal();
+    this.create.addEventListener('click', () => {
       this.modal.style.display = 'none';
-      this.currenciesBlock.insertAdjacentHTML('afterbegin', `<div class="content">${this.field.value}</div>`);
+      this.currenciesBlock.insertAdjacentHTML('afterbegin', `<div class="content">${this.field.value}<span class="remove-item">×</span></div>`);
+      this.field.value = '';
+      localStorage.setItem('test', this.board.innerHTML);
+    });
+  }
+
+  remove() {
+    document.addEventListener('click', (e) => {
+      if (e.target.classList.contains('remove-item')) {
+        e.target.parentElement.remove();
+        localStorage.setItem('test', this.board.innerHTML);
+      } else { /* eslint-disable */
+        return;
+      }
     });
   }
 }
 
 const board = new TaskBoard();
-
+board.onLoad();
 board.moving();
 board.createItem();
+board.remove();
